b9c5fbc1ad11efb46b0601443e44365d5d5d2fc5
Luiz Furtado <luizfpfurtado@hotmail.com>
2021-03-16 20:55:41 -0300

    Desafio 02 da trilha nodejs sobre middlewares - solução

--- src/index.js
+++ src/index.js
@@ -11,18 +11,61 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+    return response.status(400).json({ error: "Username already in use!" });
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if ((!user.pro && user.todos.lenght < 10) || user.pro) {
+    return next();
+  } else
+    return response.status(400).json({ error: "With a free plan, you can only have 10 todos" });
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+    return response.status(400).json({ error: "User not found!" });
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo)
+    return response.status(400).json({ error: "Todo does not exists!" });
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user)
+    return response.status(400).json({ error: "User not found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
